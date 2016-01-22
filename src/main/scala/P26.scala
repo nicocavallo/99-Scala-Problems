c8dcf5e85d48e622b6709f102abb805b5f3ae536
Nicolas Juan Cavallo Fandino <nicolas.cavallo@picsolve.com>
2016-01-22 09:06:17 +0000

    Fixed style errors

--- src/main/scala/P26.scala
+++ src/main/scala/P26.scala
@@ -1,9 +1,11 @@
 package org.p99.scala
 
 object P26 {
-  def combinations[T](n:Int, list: List[T]): List[List[T]] =
-    if (n > list.length) Nil
-    else if (n == 1) list.map(List(_))
-    else combinations(n-1,list.tail).map(list.head :: _) ::: combinations(n, list.tail)
+  def combinations[T](n:Int, list: List[T]): List[List[T]] = list match {
+    case Nil => Nil
+    case l if n > l.length => Nil
+    case l if n == 1 => l.map(List(_))
+    case _ => combinations(n-1,list.tail).map(list.head :: _) ::: combinations(n, list.tail)
+  }
 
 }
